4bbb1d860f8bcae534ba6f1e592c1c200ffd110b
Zeusse Neumbi <zeusseneumbi@gmail.com>
2026-06-21 15:12:58 +0100

    feat: wire up IPC command registration

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,19 @@
+mod commands;
+mod models;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
-        .invoke_handler(tauri::generate_handler![])
+        .invoke_handler(tauri::generate_handler![
+            commands::file::open_file,
+            commands::file::save_file,
+            commands::file::open_directory,
+            commands::file::check_file_status,
+            commands::app::get_app_version,
+            commands::app::get_app_name,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
