1647859bf76d7ac41e128b8aa634f8337c1eedb5
Zeusse Neumbi <zeusseneumbi@gmail.com>
2026-06-21 15:22:45 +0100

    feat: wire up entry point and configure Tauri permissions

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { App } from "./components/App";
 import "./styles/globals.css";
 
 const rootEl = document.getElementById("root");
 if (!rootEl) throw new Error("Root element not found");
+
 ReactDOM.createRoot(rootEl).render(
   <React.StrictMode>
-    <div>
-      <h1>MD Editor</h1>
-    </div>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 );
