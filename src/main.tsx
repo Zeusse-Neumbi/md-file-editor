593008ac24fb491b934e6ee13adab1e5552ae113
Zeusse Neumbi <zeusseneumbi@gmail.com>
2026-06-21 15:14:18 +0100

    feat: configure Tailwind v4 and preview styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./styles/globals.css";
 
 const rootEl = document.getElementById("root");
 if (!rootEl) throw new Error("Root element not found");
